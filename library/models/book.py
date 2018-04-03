ec7ba0fb425b0089b7c2102a3e147e255c086f05
= <christianadunn1990@gmail.com>
2018-04-03 06:39:50 -0700

    UPdate

--- library/models/book.py
+++ library/models/book.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class book(models.Model):
     _name = 'library.book'
@@ -12,5 +12,22 @@ class book(models.Model):
     copies = fields.Integer(string='Number of Copies', required=True, default=1)
     published_date = fields.Date(string='Date Published')
     history_ids = fields.One2many(comodel_name="library.history", inverse_name="book_id", string='History')
+    minimum_quantity = fields.Integer(string="Minimum Quantity", default=1)
+    needs_reorder = fields.Boolean(compute="_check_book_reorder", string="Needs Reorder")
+
+
+    @api.multi
+    @api.depends("copies", "minimum_quantity")
+    def _check_book_reorder(self):
+        for record in self:
+            if record.copies <= record.minimum_quantity:
+                record.needs_reorder = True
+                print(record.copies)
+            else:
+                record.needs_reorder = False
+
     
+
+
+
     
--- library/models/history.py
+++ library/models/history.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class history(models.Model):
     _name = 'library.history'
@@ -9,3 +9,13 @@ class history(models.Model):
     book_id = fields.Many2one(comodel_name="library.book", string="Book")
     customer_id = fields.Many2one(comodel_name="library.customer", string="Customer")
     checked_out = fields.Date(string="Checked Out Date")
+    returned = fields.Boolean(compute="_check_returned",store = True, string="Is Returned")
+
+    @api.multi
+    @api.depends("checked_out")
+    def _check_returned(self):
+        for record in self:
+            if record.checked_out is  not False:
+                returned = True
+            else:
+                returned = False
